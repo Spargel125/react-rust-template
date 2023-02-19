619673cd977f884ffe85a5c2e2e7e41f67990733
spargel <asupar125ce@gmail.com>
2023-02-19 16:27:01 +0900

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, {useEffect,useState} from 'react';
+import init, {add} from "wasm-lib";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [ans, setAns] = useState(0);
+useEffect(() =>{
+  init().then(() => {
+    setAns(add(1,1));
+  })
+},[])
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <p>
+          1+2 = {ans}
         </p>
         <a
           className="App-link"
